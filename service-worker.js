1217d16268cc89b26766f95e3b9bfb00af836876
StanleyRepair <hideparkproszymix@gmail.com>
2026-09-08 21:24:32 +0200

    Odśwież cache po przebudowie aplikacji

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'halloween-3-v1';
+const CACHE_NAME = 'halloween-3-v2';
 const CORE_FILES = [
   './',
   './index.html',
@@ -29,7 +29,6 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
-
   const requestUrl = new URL(event.request.url);
   if (requestUrl.origin !== self.location.origin) return;
 
